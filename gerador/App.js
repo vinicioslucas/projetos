fa246524603a12505aec41c1dd53a5bb5a6b897e
Lucas Vinícios <113357825+vinicioslucas@users.noreply.github.com>
2024-03-18 22:46:18 -0300

    criando ambiente react native
    Criando meu primeiro app com react native

---
+++ gerador/App.js
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import {View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import Slider from '@react-native-community/slider'
+
+export default function App(){
+  const [size, setSize] = useState(10)
+}
+
+export default function App(){
+  return(
+    <View style={styles.container}>
+      <Image
+        source={require("./src/assets/logo.png")}
+        style={styles.logo}
+      />
+
+      <Text style={styles.title}>20 Caracteres</Text>
+
+      <View style={styles.area}>
+        <Slider
+        styles={{ height: 50}}
+        minimumValue={6}
+        maximumValue={20}
+        maximumTrackTintColor="#ff0000"
+        minimumTrackTintColor='#000'
+        thumbTintColor='#392de9'
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.ButtonText}>Gerar Senha</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#F3F3FF',
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  logo:{
+    marginBottom: 60,
+  },
+  area:{
+    marginTop: 14,
+    marginBottom: 14,
+    width: '80%',
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    padding: 8,
+  },
+  button:{
+    backgroundColor: '#392de9',
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginBottom: 18
+  },
+  ButtonText:{
+    color: "#FFF",
+    fontSize: 20,
+  },
+  title:{
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+
+
+})
